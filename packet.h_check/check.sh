e5457a236bbe2f8251b36d5d31052f697f738139
Sven Eckelmann <sven@narfation.org>
2015-04-23 17:29:17 +0200

    packet.h_check: Use central point to configure remote
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: batman <batman@open-mesh.org>

--- packet.h_check/check.sh
+++ packet.h_check/check.sh
@@ -1,19 +1,21 @@
 #! /bin/sh
 
 TO="linux-merge@lists.open-mesh.org"
+REMOTE="/srv/git/repositories/batman-adv.git/"
+REMOTE_BATCTL="/srv/git/repositories/batctl.git/"
 
 cd /home/batman/build_test/packet.h_check
 
 rm -f batctl_maint.packet.h batctl_next.packet.h  batctl_master.packet.h  batman-adv_maint.packet.h  batman-adv_next.packet.h batman-adv_master.packet.h
 rm -f maint.diff next.diff  master.diff
 
-git --git-dir=/srv/git/repositories/batman-adv.git/ cat-file -p master:packet.h > batman-adv_master.packet.h
-git --git-dir=/srv/git/repositories/batman-adv.git/ cat-file -p next:packet.h > batman-adv_next.packet.h
-git --git-dir=/srv/git/repositories/batman-adv.git/ cat-file -p maint:packet.h > batman-adv_maint.packet.h
+git --git-dir="${REMOTE}" cat-file -p master:packet.h > batman-adv_master.packet.h
+git --git-dir="${REMOTE}" cat-file -p next:packet.h > batman-adv_next.packet.h
+git --git-dir="${REMOTE}" cat-file -p maint:packet.h > batman-adv_maint.packet.h
 
-git --git-dir=/srv/git/repositories/batctl.git/ cat-file -p master:packet.h > batctl_master.packet.h
-git --git-dir=/srv/git/repositories/batctl.git/ cat-file -p next:packet.h > batctl_next.packet.h
-git --git-dir=/srv/git/repositories/batctl.git/ cat-file -p maint:packet.h > batctl_maint.packet.h
+git --git-dir="${REMOTE_BATCTL}" cat-file -p master:packet.h > batctl_master.packet.h
+git --git-dir="${REMOTE_BATCTL}" cat-file -p next:packet.h > batctl_next.packet.h
+git --git-dir="${REMOTE_BATCTL}" cat-file -p maint:packet.h > batctl_maint.packet.h
 
 diff -ruN batman-adv_master.packet.h batctl_master.packet.h > master.diff
 diff -ruN batman-adv_next.packet.h batctl_next.packet.h > next.diff
