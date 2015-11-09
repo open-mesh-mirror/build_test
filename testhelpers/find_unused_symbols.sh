ce46e59c078b17c87759fa2e2ad4c5bd76ae9d34
Build Test <build_test@open-mesh.org>
2015-11-09 22:11:26 +0100

    Add new allowed unused symbols for batman V

--- testhelpers/find_unused_symbols.sh
+++ testhelpers/find_unused_symbols.sh
@@ -14,7 +14,7 @@ path="$(source_path)"
 defined="`nm -g --defined-only "${path}"/*.o|awk '{ print $3}'|sort|uniq`"
 used="`nm -g --undefined-only "${path}"/*.o|awk '{ print $2}'|sort|uniq`"
 ret=0
-blacklist="cleanup_module batadv_hash_set_lock_class batadv_send_skb_prepare_unicast_4addr batadv_unicast_4addr_prepare_skb batadv_skb_crc32"
+blacklist="cleanup_module batadv_hash_set_lock_class batadv_send_skb_prepare_unicast_4addr batadv_unicast_4addr_prepare_skb batadv_skb_crc32 batadv_send_skb_packet batadv_parse_throughput"
 
 for i in $defined; do
 	found=0
