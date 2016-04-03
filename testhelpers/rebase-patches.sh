82be75a9b2d6c1b5551749b2fbc6f7ff8081c9e5
Sven Eckelmann <sven@narfation.org>
2016-04-03 09:39:25 +0200

    build_test: Rebase patches to apply cleanly

---
+++ testhelpers/rebase-patches.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+set -e
+
+LINUX_REPOSITORY=${LINUX_REPOSITORY:="$HOME/tmp/linux-next/"}
+LINUX_VERSIONS=$(echo linux-3.{2..19} linux-4.{0..5})
+PATCHDIR="$(pwd)/../patches/"
+
+
+for i in ${LINUX_VERSIONS}; do
+	version="$(echo "${i}"|sed 's/^linux-/v/')"
+
+	if [ ! -d "${PATCHDIR}/${version}/" ]; then
+		continue
+	fi
+
+	cd "${LINUX_REPOSITORY}"
+	git checkout "${version}"
+	git am "${PATCHDIR}/${version}/"*
+	rm "${PATCHDIR}/${version}/"*
+	git format-patch --output-directory "${PATCHDIR}/${version}/" --diff-algorithm=histogram --no-signature --format=format:'From: %an <%ae>%nDate: %aD%nSubject: [PATCH] %B' "${version}"
+done
