c19523a352ad448cb72688730e99297fe8e166fd
Sven Eckelmann <sven@narfation.org>
2016-05-13 17:56:41 +0200

    build_test: Add helper to check the state of batman_adv.h

---
+++ batman_adv.h_check/check.sh
@@ -0,0 +1,54 @@
+#! /bin/sh
+
+TO="linux-merge@lists.open-mesh.org"
+REMOTE="/srv/git/repositories/batman-adv.git/"
+REMOTE_BATCTL="/srv/git/repositories/batctl.git/"
+
+cd "$(dirname "$0")"
+
+rm -f batctl_maint.batman_adv.h batctl_next.batman_adv.h  batctl_master.batman_adv.h  batman-adv_maint.batman_adv.h  batman-adv_next.batman_adv.h batman-adv_master.batman_adv.h
+rm -f maint.diff next.diff  master.diff
+
+checkout_batman_adv_h()
+{
+	remote="${1}"
+	branch="${2}"
+
+	git --git-dir="${remote}" cat-file -p "${branch}":include/uapi/linux/batman_adv.h 2> /dev/null
+	if [ "$?" = "0" ]; then
+		return
+	fi
+
+	git --git-dir="${remote}" cat-file -p "${branch}":batman_adv.h 2> /dev/null
+}
+
+checkout_batman_adv_h "${REMOTE}" master > batman-adv_master.batman_adv.h
+checkout_batman_adv_h "${REMOTE}" next > batman-adv_next.batman_adv.h
+checkout_batman_adv_h "${REMOTE}" maint > batman-adv_maint.batman_adv.h
+
+checkout_batman_adv_h "${REMOTE_BATCTL}" master > batctl_master.batman_adv.h
+checkout_batman_adv_h "${REMOTE_BATCTL}" next > batctl_next.batman_adv.h
+checkout_batman_adv_h "${REMOTE_BATCTL}" maint > batctl_maint.batman_adv.h
+
+diff -ruN batman-adv_master.batman_adv.h batctl_master.batman_adv.h > master.diff
+diff -ruN batman-adv_next.batman_adv.h batctl_next.batman_adv.h > next.diff
+diff -ruN batman-adv_maint.batman_adv.h batctl_maint.batman_adv.h > maint.diff
+
+generate_email_header()
+{
+	cat <<-EOF
+	From: postmaster@open-mesh.org
+	To: $1
+	Subject: Noticed difference in batman_adv.h in $2
+	MIME-Version: 1.0
+	Content-Type: text/plain; charset=UTF-8
+
+	EOF
+}
+
+for i in master.diff next.diff maint.diff; do
+	if [ -s "$i" ]; then
+		(generate_email_header "$TO" "$i"
+		cat "$i") | /usr/sbin/sendmail -t
+	fi
+done
