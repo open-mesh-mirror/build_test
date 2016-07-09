d586265cddd330cb5a35b6c9fdff7c92124a179e
Sven Eckelmann <sven@narfation.org>
2016-07-10 00:58:42 +0200

    build_test: Show statistics per branch

--- testhelpers/mail_aggregator.py
+++ testhelpers/mail_aggregator.py
@@ -12,7 +12,7 @@ def usage(prog):
 	print >> sys.stderr, '%s FILE COMMAND\n' % (prog)
 	print >> sys.stderr, 'COMMAND:'
 	print >> sys.stderr, '\tcreate'
-	print >> sys.stderr, '\tadd NAME LOGFILE LONGLOGFILE'
+	print >> sys.stderr, '\tadd BRANCH NAME LOGFILE LONGLOGFILE'
 	print >> sys.stderr, '\tsend FROM TO TOPIC'
 	print >> sys.stderr, '\tadd_buildtests BRANCH VERSION CONFIG'
 	sys.exit(1)
@@ -25,7 +25,7 @@ def create():
 
 	con = sqlite3.connect(dbfile)
 	cur = con.cursor()
-	cur.execute('CREATE TABLE logs (name, log, longlog)')
+	cur.execute('CREATE TABLE logs (branch, name, log, longlog)')
 	cur.execute('CREATE TABLE buildtests (branch, version, config)')
 	cur.execute('CREATE UNIQUE INDEX buildtests_unique ON buildtests(branch, version, config)')
 	con.commit()
@@ -33,16 +33,17 @@ def create():
 
 def add():
 	dbfile = sys.argv[1];
-	name = sys.argv[3]
-	logfile = sys.argv[4]
-	longlogfile = sys.argv[5]
+	branch = sys.argv[3]
+	name = sys.argv[4]
+	logfile = sys.argv[5]
+	longlogfile = sys.argv[6]
 
 	log = open(logfile).read()
 	longlog = open(longlogfile).read()
 
 	con = sqlite3.connect(dbfile)
 	cur = con.cursor()
-	cur.execute('INSERT INTO logs VALUES (?, ?, ?)', (name, log, longlog))
+	cur.execute('INSERT INTO logs VALUES (?, ?, ?, ?)', (branch, name, log, longlog))
 	con.commit()
 	cur.close()
 
@@ -62,45 +63,64 @@ def add_buildtests():
 	except sqlite3.IntegrityError:
 		sys.exit(1)
 
+def add_branchescounter_to_set(branchset, counter):
+	for c in counter:
+		branchset.add(c[0])
+
+def get_branchescounter(branch, counter):
+	for c in counter:
+		if c[0] == branch:
+			return c[1]
+
+	return 0
+
 def send():
 	dbfile = sys.argv[1];
 	mail_from = sys.argv[3]
 	mail_to = sys.argv[4]
 	mail_subject = sys.argv[5]
+	branches = set()
 
 	con = sqlite3.connect(dbfile)
 	cur = con.cursor()
-	cur.execute('SELECT name FROM logs ORDER BY name')
+	cur.execute('SELECT branch,name FROM logs ORDER BY name')
 	names = cur.fetchall()
-	cur.execute('SELECT name,log FROM logs GROUP BY log ORDER BY name')
+	cur.execute('SELECT branch,name,log FROM logs GROUP BY log ORDER BY name')
 	logs = cur.fetchall()
-	cur.execute('SELECT COUNT(*) FROM buildtests')
-	buildcount = cur.fetchall()[0][0]
-	cur.execute('SELECT COUNT(DISTINCT version) FROM buildtests')
-	versioncount = cur.fetchall()[0][0]
-	cur.execute('SELECT COUNT(DISTINCT config) FROM buildtests')
-	configcount = cur.fetchall()[0][0]
+	cur.execute('SELECT branch, COUNT(*) FROM logs group by branch')
+	failcount = cur.fetchall()
+	cur.execute('SELECT branch, COUNT(*) FROM buildtests group by branch')
+	buildcount = cur.fetchall()
+	cur.execute('SELECT branch, COUNT(DISTINCT version) FROM buildtests group by branch')
+	versioncount = cur.fetchall()
+	cur.execute('SELECT branch, COUNT(DISTINCT config) FROM buildtests group by branch')
+	configcount = cur.fetchall()
 	cur.close()
 
 	if len(names) == 0:
 		return
 
+	add_branchescounter_to_set(branches, failcount)
+	add_branchescounter_to_set(branches, buildcount)
+	add_branchescounter_to_set(branches, versioncount)
+	add_branchescounter_to_set(branches, configcount)
+
 	name_list = []
 	for x in names:
-		name_list.append(" * " + x[0])
+		name_list.append(" * %s: %s" % (x[0], x[1]))
 
 	log_list = []
 	for x in logs:
-		namelen = len(x[0])
-		if namelen > 75:
-			namelen = 75
+		name = "%s: %s" % (x[0], x[1])
+		namelen = len(name)
+		namelen = min(namelen, 75)
 
 		underline = '-' * namelen
 
-		full_log = re.sub('^', '    ', x[1], count = 0, flags = re.MULTILINE)
+		full_log = re.sub('^', '    ', x[2], count = 0, flags = re.MULTILINE)
 		full_log = re.sub('\s*$', '', full_log, count = 0)
 
-		log_list.append("%s\n%s\n\n%s" % (x[0], underline, full_log))
+		log_list.append("%s\n%s\n\n%s" % (name, underline, full_log))
 
 	mail = []
 	mail.append("Name of failed tests\n")
@@ -116,10 +136,24 @@ def send():
 	mail.append("Statistics\n")
 	mail.append("==========\n")
 	mail.append("\n")
-	mail.append("Failed tests:          %8u\n" % (len(name_list)))
-	mail.append("Started build tests:   %8u\n" % (buildcount))
-	mail.append("Tested Linux versions: %8u\n" % (versioncount))
-	mail.append("Tested configs:        %8u\n" % (configcount))
+
+	for b in branches:
+		namelen = len(b)
+		namelen = min(namelen, 75)
+		underline = '-' * namelen
+
+		mail.append("\n%s\n%s\n\n" % (b, underline))
+
+		failcnt = get_branchescounter(b, failcount)
+		buildcnt = get_branchescounter(b, buildcount)
+		versioncnt = get_branchescounter(b, versioncount)
+		configcnt = get_branchescounter(b, configcount)
+		mail.append("Failed tests:          %8u\n" % (failcnt))
+		mail.append("Started build tests:   %8u\n" % (buildcnt))
+		mail.append("Tested Linux versions: %8u\n" % (versioncnt))
+		mail.append("Tested configs:        %8u\n" % (configcnt))
+
+	mail.append("\n")
 
 	msg = MIMEText("".join(mail))
 	msg['Subject'] = mail_subject
@@ -145,7 +179,7 @@ def main():
 
 	if sys.argv[2] == 'create' and len(sys.argv) == 3:
 		create()
-	elif sys.argv[2] == 'add' and len(sys.argv) == 6:
+	elif sys.argv[2] == 'add' and len(sys.argv) == 7:
 		add()
 	elif sys.argv[2] == 'add_buildtests' and len(sys.argv) == 6:
 		add_buildtests()
