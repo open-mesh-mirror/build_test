b7ab96a73e640e9e81032bb5455bc1681531515e
Sven Eckelmann <sven@narfation.org>
2017-11-12 21:01:49 +0100

    build_test: Add support for Linux 4.14

--- testhelpers/rebase-patches.sh
+++ testhelpers/rebase-patches.sh
@@ -2,7 +2,7 @@
 set -e
 
 LINUX_REPOSITORY=${LINUX_REPOSITORY:="$HOME/tmp/linux-next/"}
-LINUX_VERSIONS=$(echo linux-3.{2..19} linux-4.{0..13})
+LINUX_VERSIONS=$(echo linux-3.{2..19} linux-4.{0..14})
 PATCHDIR="$(pwd)/../patches/"
 
 
