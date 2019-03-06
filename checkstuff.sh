open-mesh-mirror/build_test

commit be19d7184fc377a49c6aed09d02900c4f9858a40

build_test: Drop header changes in compat-includes

checkstuff.sh
@@ -468,7 +468,7 @@ test_headers()
 		"${FIX_INCLUDE_SORT}" --nosafe_headers --separate_project_includes="$(pwd)/${bpath}" < test
 
 		# remove extra noise
-		git checkout -f -- compat-sources
+		git checkout -f -- compat-sources compat-includes
 		sed -i '/^struct batadv_algo_ops;$/d' "${bpath}"/main.h
 		sed -i '/^struct batadv_hard_iface;$/d' "${bpath}"/main.h
 		sed -i '/^struct batadv_orig_node;$/d' "${bpath}"/main.h

commit aa07515067035ff5c04bd09692f4ccb3a72ffc27

build_test: Fix cleanup for compat-* header noise
Fixes: be19d7184fc3 ("build_test: Drop header changes in compat-includes")

checkstuff.sh
@@ -468,7 +468,8 @@ test_headers()
 		"${FIX_INCLUDE_SORT}" --nosafe_headers --separate_project_includes="$(pwd)/${bpath}" < test
 
 		# remove extra noise
-		git checkout -f -- compat-sources compat-includes
+		git checkout -f -- compat-include
+		git checkout -f -- compat-sources
 		sed -i '/^struct batadv_algo_ops;$/d' "${bpath}"/main.h
 		sed -i '/^struct batadv_hard_iface;$/d' "${bpath}"/main.h
 		sed -i '/^struct batadv_orig_node;$/d' "${bpath}"/main.h