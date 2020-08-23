3aa0d962a9adefe72e771aa4f90c7d2447806154
Sven Eckelmann <sven@narfation.org>
2020-08-23 20:28:17 +0200

    build_test: Switch to netdev's git trees
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- checkstuff.sh
+++ checkstuff.sh
@@ -65,8 +65,8 @@ check_external()
 	if [ ! -x "${CHECKPATCH}" -o ! -x "${KERNELDOC}" ]; then
 		echo "Required tool checkpatch and kernel-doc missing:"
 		echo "    git clone --depth=1 git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git linux-next"
-		echo "    git --git-dir=linux-next/.git/ remote add -t master --no-tags net-next git://git.kernel.org/pub/scm/linux/kernel/git/davem/net-next.git"
-		echo "    git --git-dir=linux-next/.git/ remote add -t master --no-tags net git://git.kernel.org/pub/scm/linux/kernel/git/davem/net.git"
+		echo "    git --git-dir=linux-next/.git/ remote add -t master --no-tags net-next https://git.kernel.org/pub/scm/linux/kernel/git/netdev/net-next.git"
+		echo "    git --git-dir=linux-next/.git/ remote add -t master --no-tags net https://git.kernel.org/pub/scm/linux/kernel/git/netdev/net.git"
 		echo "    git --git-dir=linux-next/.git/ config remote.origin.tagopt --no-tags"
 		echo "    git --git-dir=linux-next/.git/ fetch --depth=1 net-next"
 		echo "    git --git-dir=linux-next/.git/ fetch --depth=1 net"
