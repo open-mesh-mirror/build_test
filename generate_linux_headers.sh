9b18f8d01ed87bbc8e8051f4367da045216f6d26
Sven Eckelmann <sven@narfation.org>
2026-05-17 09:30:55 +0200

    build_test: Update smatch to 1.75
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- generate_linux_headers.sh
+++ generate_linux_headers.sh
@@ -227,7 +227,7 @@ prepare_smatch()
 	outpath="linux-build/smatch/"
 
 	git clone https://repo.or.cz/smatch.git/ "${outpath}"
-	git -C "${outpath}" reset --hard b810ac53abab7b93ac62a74cf805ac111b849284
+	git -C "${outpath}" reset --hard 5e13a0ee549b29edc4fd5b92579a49acf9194b63
 	git -C "${outpath}" am ../../patches/smatch/0001-smatch-disable-verbose-check_unused_ret.patch
 	CFLAGS="-march=native -O3" make -C "${outpath}"
 }
